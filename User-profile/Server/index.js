cc35efb58c595974005ff492456792b53ac677ec
romiyadangol <dangolromiya@gmail.com>
2024-07-05 22:18:16 +0545

    Added form

--- User-profile/Server/index.js
+++ User-profile/Server/index.js
@@ -1,17 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req,res) => {
+
+app.get('/', (req, res) => {
     res.render("profile", {
-        name : "John Doe",
-        age : 25,
-        isAdmin : true,
-        hobbies : ['Reading', 'Traveling', 'Cooking']
-    })
-})
+    name: "John Doe",
+    age: 25,
+    isAdmin: true,
+    hobbies: ['Reading', 'Traveling', 'Cooking'],
+    payload:{}
+    });
+});
+
+app.post('/profile', (req, res) => {
+    const payload = req.body;
+    const updateData = {
+        name: payload.name,
+        age: payload.age,
+        isAdmin: payload.isAdmin === 'on',
+        hobbies: payload.hobbies ? payload.hobbies.split(',').map(hobby => hobby.trim()) : [],
+    };
+    res.render("profile", {
+        ...updateData,
+        payload: payload,
+    });
+});
+
 
 app.listen(8000, () => {
     console.log("Server is running on PORT 8000");
-})
+});
